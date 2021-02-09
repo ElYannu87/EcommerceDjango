82847bff091380bb81b9860a14b1a113b57c0620
ElYannu87 <y.driever@gmail.com>
2021-02-09 12:29:37 +0100

    added cart, checkout and new functions

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
